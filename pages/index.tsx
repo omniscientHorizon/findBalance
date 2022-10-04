09890c345995369bc77867b23c81addcee38ee1a
Horizon <omniscienthorizon@gmail.com>
2022-10-05 01:39:33 +0530

    reduced repeated lines of code removed 3 useless functions

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,45 +8,43 @@ import * as Web3 from '@solana/web3.js'
 import { checkIsManualRevalidate } from 'next/dist/server/api-utils'
 import { get } from 'https'
 
-
-
 const Home: NextPage = () => {
   const [balance, setBalance] = useState(0)
+  const [Tbalance, setTBalance] = useState(0)
+  const [Dbalance, setDBalance] = useState(0)
   const [address, setAddress] = useState('')
   const [chainName, setchainName] = useState('mainnet-beta')
   const [isExecutable, setExecutable] = useState(false)
 
-  const makeItMainnet = () =>{
-    console.log('mainnet karne ka hai')
-    setchainName('mainnet-beta')
-  }
-  
-  const makeItDevnet = () =>{
-    console.log('devnet karne ka hai')
-    setchainName('devnet')
-  }
-
-  const makeItTestnet = () =>{
-    console.log('testnet karne ka hai')
-    setchainName('testnet')
-  }
-
-  const chainname: Web3.Cluster = chainName
 
   const addressSubmittedHandler = (address: string) => {
 
     try {
     const key = new Web3.PublicKey(address)
     setAddress(key.toBase58())
-    const connection = new Web3.Connection(Web3.clusterApiUrl(chainname))
-
-    
+    let connection = new Web3.Connection(Web3.clusterApiUrl('mainnet-beta'))
+    //setting mainnet balance
     connection.getBalance(key).then (balance => {
       setBalance(balance / Web3.LAMPORTS_PER_SOL)
-      if((balance / Web3.LAMPORTS_PER_SOL)==0){
-        alert('seems you have no money :(')}
+      // if((balance / Web3.LAMPORTS_PER_SOL)==0){
+      //   alert('seems you have no money on mainnet :(')}
       })
-        
+
+    connection = new Web3.Connection(Web3.clusterApiUrl('devnet'))
+    //setting devnet balance
+    connection.getBalance(key).then (balance => {
+      setDBalance(balance / Web3.LAMPORTS_PER_SOL)
+      // if((balance / Web3.LAMPORTS_PER_SOL)==0){
+      //   alert('seems you have no money on devnet :(')}
+      })
+    
+    connection = new Web3.Connection(Web3.clusterApiUrl('devnet'))
+    //setting testnet balance
+    connection.getBalance(key).then (balance => {
+      setTBalance(balance / Web3.LAMPORTS_PER_SOL)
+      // if((balance / Web3.LAMPORTS_PER_SOL)==0){
+      //   alert('seems you have no money on testnet:(')}
+      })
 
     connection.getAccountInfo(key).then (isExecutable => {
       setExecutable(isExecutable?.executable ?? false)
@@ -66,23 +64,22 @@ const Home: NextPage = () => {
         <h1 > <em>Hola</em>👋</h1>  
         <div className={styles.Rules}>
         <p className={styles.opacityIncrease}>
-          Let&aposs find out who has how much $SOL 🤑
+          Let's find out who has how much $SOL 🤑
         </p>
         <ol className={styles.opacityIncrease}>
           <li>Enter a public key</li>
-          <li>Select the chain</li>
-          <li>Check balance</li>
+          <li>Click check balance</li>
         </ol>
 
         </div>
         <AddressForm handler={addressSubmittedHandler} />
         <div className={styles.buttonHolder}>
-        <button className={styles.mainnetButton} onClick={makeItMainnet}>mainnet</button>
-        <button className={styles.devnetButton} onClick={makeItDevnet}>devnet</button>
-        <button className={styles.testnetButton} onClick={makeItTestnet}>testnet</button>
+        <button className={styles.mainnetButton} disabled>mainnet ${balance}</button>
+        <button className={styles.devnetButton} >devnet ${Dbalance}</button>
+        <button className={styles.testnetButton} >testnet ${Tbalance}</button>
         </div>
         <p>{`Address: ${address}`} <br/>
-        {`Balance: ${balance} SOL on ${chainName}`}</p>
+        </p>
         {/* <p>{`Is it executable: ${isExecutable? 'Yupp':'Nuh-uh'}`}</p> */}
       </header>
     </div>
